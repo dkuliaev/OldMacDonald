8573171bb36492486b03bbbb92ed8c8903cb1dca
dkuliaev <dkuliaev@gmail.com>
2015-10-27 11:00:16 -0700

    Dmitry's finihsed OldMacDonald

--- Chick.java
+++ Chick.java
@@ -1,4 +1,21 @@
 class Chick implements Animal 
 {     
-     //your code here
+     private String myType;     
+     private String mySound;  
+     private int chickSound = (int)(Math.random() * 2);     
+     public Chick(String type, String sound)    
+     {         
+         myType = type;         
+         mySound = sound;
+          
+     }     
+     public Chick()     {         
+        myType = "Chick";
+        if(chickSound == 0)       
+            mySound = "Cluck!";
+        if(chickSound == 1)
+            mySound = "Cheep!";
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;}
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,5 @@
-
-void setup()
-{
-	//your variable declarations here
+public void setup() 
+{     
+	Farm donald = new Farm();
+	donald.animalSounds();
 }
-
